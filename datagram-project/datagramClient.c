91dd65bb3bd9330025245dcf14921b555e49d075
andrioluss <56278628+andrioluss@users.noreply.github.com>
2019-10-30 19:02:28 +0100

    Aggiunta commenti

--- datagram-project/datagramClient.c
+++ datagram-project/datagramClient.c
@@ -11,15 +11,11 @@
 
 #define LINE_LENGTH 256
 
+//Codici errore
 #define EXIT_ARGS 1
 #define EXIT_HOST_ERR 2
 
-/*Struttura di una richiesta*/
-/********************************************************/
-typedef struct{
-	char nomefile[LINE_LENGTH];
-}Request;
-/********************************************************/
+// client serverAddress portAddress
 
 int main(int argc, char **argv)
 {
@@ -30,7 +26,7 @@ int main(int argc, char **argv)
 	int lenNomeFile;
 	int lenAddress;
 	int ris;
-	Request req;
+	char nomefile[LINE_LENGTH];
 
 	int i = 0;
 
@@ -40,28 +36,15 @@ int main(int argc, char **argv)
 		exit(EXIT_ARGS);
 	}
 
-/* INIZIALIZZAZIONE INDIRIZZO CLIENT E SERVER --------------------- */
-	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
-	clientaddr.sin_family = AF_INET;
-	clientaddr.sin_addr.s_addr = INADDR_ANY;
-	
-	/* Passando 0 ci leghiamo ad un qualsiasi indirizzo libero,
-	* ma cio' non funziona in tutti i sistemi.
-	*/
-	clientaddr.sin_port = 0;
-
-	//Recupero l'indirizzo del server remoto attraverso la funzione gethostbyname.
-	host = gethostbyname (argv[1]);
-
 	//Controllo sulla porta
-		while( argv[2][i]!= '\0' ){
-			if((argv[2][i] < '0') || (argv[2][i] > '9')){
-				printf("Secondo argomento non intero\n");
-				printf("Error:%s serverAddress serverPort\n", argv[0]);
-				exit(EXIT_ARGS);
-			}
-			i++;
-		}  
+	while( argv[2][i]!= '\0' ){
+		if((argv[2][i] < '0') || (argv[2][i] > '9')){
+			printf("Secondo argomento non intero\n");
+			printf("Error:%s serverAddress serverPort\n", argv[0]);
+			exit(EXIT_ARGS);
+		}
+		i++;
+	} 
 
 	//Conversione port a intero
 	port = atoi(argv[2]);
@@ -71,6 +54,18 @@ int main(int argc, char **argv)
 		printf("%s = porta scorretta...\n", argv[2]);
 		exit(EXIT_ARGS);
 	}
+
+	/* INIZIALIZZAZIONE INDIRIZZO CLIENT E SERVER --------------------- */
+	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr = INADDR_ANY;
+	
+	// Passando 0 ci leghiamo ad un qualsiasi indirizzo libero
+	clientaddr.sin_port = 0;
+
+	//Recupero l'indirizzo del server remoto attraverso la funzione gethostbyname.
+	host = gethostbyname (argv[1]); 
+
 	//Verifica host
 	if (host == NULL){
 		printf("%s not found in /etc/hosts\n", argv[1]);
@@ -79,40 +74,45 @@ int main(int argc, char **argv)
 		//Inizializzazione server address
 		memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
 		servaddr.sin_family = AF_INET;
-		servaddr.sin_addr.s_addr=((struct in_addr *)(host->h_addr))->s_addr;
+		servaddr.sin_addr.s_addr=((struct in_addr *)(host->h_addr_list[0]))->s_addr;
 		servaddr.sin_port = htons(port);
 	}
 
-	/* CREAZIONE SOCKET ---------------------------------- */
-	sd=socket(AF_INET, SOCK_DGRAM, 0);
+	/* CREAZIONE (e controllo) SOCKET ---------------------------------- */
 
-	if(sd<0) {perror("apertura socket"); exit(1);}
+	if((sd=socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+		perror("apertura socket");
+		exit(1);
+	}
 	printf("Client: creata la socket sd=%d\n", sd);
 
 	/* BIND SOCKET, a una porta scelta dal sistema --------------- */
-	if(bind(sd,(struct sockaddr *) &clientaddr, sizeof(clientaddr))<0)
-	{perror("bind socket "); exit(1);}
+	if(bind(sd,(struct sockaddr *) &clientaddr, sizeof(clientaddr))<0) {
+		perror("bind socket ");
+		exit(1);
+	}
 	printf("Client: bind socket ok, alla porta %i\n", clientaddr.sin_port);
 
 	/* CORPO DEL CLIENT: ciclo di accettazione di richieste da utente */
 	printf("nome file, EOF per terminare: ");
 
-	while (gets(req.nomefile) != NULL )
+	while (gets(nomefile) != NULL )
 	{
     	
-		printf("Nome file: %s\n", req.nomefile);
+		printf("Nome file: %s\n", nomefile);
 
 		/* richiesta operazione */
 
 		//lunghezza nome file:
 		//Più 1 per il terminatore.
-		lenNomeFile = strlen(req.nomefile)+1;
+		lenNomeFile = strlen(nomefile)+1;
 
 		//Lunghezza struttura indirizzo.
 		lenAddress=sizeof(servaddr);
 
-		if(sendto(sd, &req, sizeof(char)*lenNomeFile, 0, (struct sockaddr *)&servaddr, lenAddress)<0){
+		if(sendto(sd, nomefile, sizeof(char)*lenNomeFile, 0, (struct sockaddr *)&servaddr, lenAddress)<0){
 			perror("sendto");
+			printf("nome file, EOF per terminare: ");
 			continue;
 		}
 
@@ -120,10 +120,13 @@ int main(int argc, char **argv)
 		printf("Attesa del risultato...\n");
 		
 		if (recvfrom(sd, &ris, sizeof(ris), 0, (struct sockaddr *)&servaddr, &lenAddress)<0){
-			perror("recvfrom"); continue;}
+			perror("recvfrom");
+			printf("nome file, EOF per terminare: ");
+			continue;
+		}
 
 		//Conversione in formato locale.
-		ris = (int)ntohs(ris);
+		ris = (int) ntohs(ris);
 
 		if(ris > 0){
 			printf("Numero dei caratteri della parola più lunga : %i\n", ris);
